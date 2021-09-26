f47158ab991d18db235e272eaf2ece5d1b9f8105
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-09-26 20:04:23 +0200

    teljes_nev

--- UserMaintence/UserMaintence/Form1.cs
+++ UserMaintence/UserMaintence/Form1.cs
@@ -19,7 +19,7 @@ namespace UserMaintence
         {
             InitializeComponent();
             label1.Text = Resource1.LastName; 
-            label2.Text = Resource1.FirstName; 
+             
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
@@ -31,8 +31,8 @@ namespace UserMaintence
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text + " " + textBox2.Text
+                
             };
             users.Add(u);
         }
