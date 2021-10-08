6bcc66bb5ac2ac3fac80388d9cff76815e67ab44
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-10-08 15:20:53 +0200

    alapok

--- negyedikhet/negyedikhet/Form1.Designer.cs
+++ negyedikhet/negyedikhet/Form1.Designer.cs
@@ -30,12 +30,44 @@ namespace negyedikhet
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.flatBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.realEstateDataSet = new negyedikhet.RealEstateDataSet();
+            this.flatTableAdapter = new negyedikhet.RealEstateDataSetTableAdapters.FlatTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.flatBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.realEstateDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flatBindingSource
+            // 
+            this.flatBindingSource.DataMember = "Flat";
+            this.flatBindingSource.DataSource = this.realEstateDataSet;
+            // 
+            // realEstateDataSet
+            // 
+            this.realEstateDataSet.DataSetName = "RealEstateDataSet";
+            this.realEstateDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // flatTableAdapter
+            // 
+            this.flatTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.flatBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.realEstateDataSet)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private RealEstateDataSet realEstateDataSet;
+        private System.Windows.Forms.BindingSource flatBindingSource;
+        private RealEstateDataSetTableAdapters.FlatTableAdapter flatTableAdapter;
     }
 }
 
