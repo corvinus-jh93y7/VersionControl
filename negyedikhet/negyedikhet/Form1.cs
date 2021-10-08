6a3ee6105be2ba35ed4b4ad485c51c02483a6515
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-10-08 15:35:31 +0200

    elvkesz

--- negyedikhet/negyedikhet/Form1.cs
+++ negyedikhet/negyedikhet/Form1.cs
@@ -24,11 +24,33 @@ namespace negyedikhet
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             Flats = context.Flat.ToList();
+        }
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                CreateTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
 
