18b10a50292ac72e7017cd940dc3873699e2cb3e
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-10-19 19:11:46 +0200

    portfolio_item_letrehoz

--- value_at_risk/value_at_risk/Form1.cs
+++ value_at_risk/value_at_risk/Form1.cs
@@ -12,9 +12,14 @@ namespace value_at_risk
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            
+            Ticks = context.Tick.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
