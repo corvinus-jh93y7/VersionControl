65bb8c046e3d370e69e1e5de3d1989bb1be1221a
corvinus-jh93y7 <matyas.novothny@stud.uni-corvinus.hu>
2021-10-26 10:58:56 +0200

    5.fel

--- soap_mnb/soap_mnb/Form1.cs
+++ soap_mnb/soap_mnb/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace soap_mnb
 {
@@ -21,7 +22,7 @@ namespace soap_mnb
         {
             InitializeComponent();
 
-
+           
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
@@ -32,6 +33,33 @@ namespace soap_mnb
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
             dataGridView1.DataSource = Rates;
+
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit !=0)
+                {
+                    rate.Value = value/unit;
+                }
+
+            }
+
+
+
         }
 
         
